2e03d3fc11d47eb27788ee79f5b3c9d3d574da3b
klkalpana <klkalpana@gmail.com>
2025-03-26 16:57:45 +0530

    Add email classifier UI and configure CORS in FastAPI

--- code/src/main.py
+++ code/src/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, UploadFile, File
-from document_processor import DocumentProcessor
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 import os
 
+from document_processor import DocumentProcessor
+
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500"],  # Allow requests from live server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/process-email")
 async def process_email(file: UploadFile = File(...)):
